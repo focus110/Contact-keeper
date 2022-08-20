50c1be800ecc9c74b4c8c43ca7c450a59e3bf1a0
focus110 <iamemmaochuko@gmail.com>
2022-08-20 09:36:17 +0100

    setup global state

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,24 @@ import Navbar from "../src/components/layout/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { About, Home } from "./pages/index";
+import ContactState from "./context/contact/ContactState";
 
 function App() {
   return (
-    <BrowserRouter>
-      <>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={"404 Not Found"} />
-          </Routes>{" "}
-        </div>
-      </>
-    </BrowserRouter>
+    <ContactState>
+      <BrowserRouter>
+        <>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={"404 Not Found"} />
+            </Routes>
+          </div>
+        </>
+      </BrowserRouter>
+    </ContactState>
   );
 }
 
