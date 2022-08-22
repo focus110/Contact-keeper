02b7e235cca374828c72a8d4c3e0c0560e1527ea
focus110 <iamemmaochuko@gmail.com>
2022-08-22 13:32:08 +0100

    prepare for deploy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const auth = require("./routes/auth");
 const users = require("./routes/users");
@@ -21,5 +22,14 @@ app.get("/", (req, res) => res.send("Server up 5000"));
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/contacts", contacts);
+
+// Serve static assets in production
+if (process.envNODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log("Server started on port " + PORT));
