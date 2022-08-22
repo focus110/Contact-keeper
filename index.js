db597328c8b0d15a628bbbe1808c7327820da14f
focus110 <iamemmaochuko@gmail.com>
2022-08-22 14:08:46 +0100

    removed initial route

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Server up 5000"));
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/contacts", contacts);
